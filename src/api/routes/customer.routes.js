b5c418629955a4ce0c3ca2c815e3a6635eb94742
gaurav-132 <ggusain265@gmai.com>
2024-08-18 22:20:31 +0530

    change

--- src/api/routes/customer.routes.js
+++ src/api/routes/customer.routes.js
@@ -2,10 +2,6 @@ import express from "express";
 import { validate } from "../middleware/validate.middleware.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 import { createCustomer, loanReq } from "../controllers/customer.controller.js";
-import {
-    createCustomer,
-    getCustomers,
-} from "../controllers/customer.controller.js";
 import { customerSchema } from "../validators/customer.validator.js";
 
 const router = express.Router();
@@ -14,6 +10,6 @@ router.route("/create-customer").post(verifyJwt, validate(customerSchema), creat
 
 router.route("/raise-loan-request").post(verifyJwt, validate(), loanReq);
 
-router.route("/get-customers").post(verifyJwt, getCustomers);
+// router.route("/get-customers").post(verifyJwt, getCustomers);
 
 export { router as customerRoutes };
