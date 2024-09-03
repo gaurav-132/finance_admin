7ad8515846591785506de3687b1e774f954fd240
Gaurav-dotcom <pokhriyalgaurav94@gmail.com>
2024-09-03 22:09:07 +0530

    edit location api

--- src/api/validators/location.validator.js
+++ src/api/validators/location.validator.js
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 const addLocationSchema = z.object({
+  id: z.number().optional(),
   locationName: z
     .string({ required_error: "Location Name is required" })
     .trim(),
